9b9a17c4fe392c6f36e5aec8b557d5483da77e57
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-09-01 02:05:52 +0100

    Create Env.py

---
+++ Env.py
@@ -0,0 +1,9 @@
+from google import genai
+
+client = genai.Client(api_key="YOUR_API_KEY")
+
+interaction = client.interactions.create(
+    model="gemini-3.7-flash",
+    input="Explain how AI works in a few words"
+)
+print(interaction.output_text)
